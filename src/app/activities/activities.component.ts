3f4d015266fd1a8ccf6750ac045b630d17dbf777
Alex <ajonp@alexs-mbp.lan>
2020-08-05 10:01:44 -0400

    remove extra columns on small

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -12,6 +12,7 @@ import { MatSort } from '@angular/material/sort';
 import { MatSelectChange } from '@angular/material/select';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { MatDialog } from '@angular/material/dialog';
+import {MediaObserver} from '@angular/flex-layout';
 
 @Component({
   selector: 'app-activities',
@@ -36,7 +37,7 @@ export class ActivitiesComponent implements OnInit {
   activities$: Observable<any>;
   dataSource = new MatTableDataSource<Activity>();
   search$ = new BehaviorSubject(null);
-  displayedColumns: string[] = ['activity', 'points', 'location', 'submit'];
+  displayedColumns$: Observable<string[]>;
 
   activityFilter = '';
   pointsFilter = 0;
@@ -45,9 +46,17 @@ export class ActivitiesComponent implements OnInit {
   @Input() team: Team;
 
   constructor(private firestore: AngularFirestore, private bottomSheet: MatBottomSheet,
-              public dialog: MatDialog) {}
+              public dialog: MatDialog, public mediaObserver: MediaObserver) {}
 
   ngOnInit(): void {
+    this.displayedColumns$ = this.mediaObserver.asObservable().pipe(map(media =>
+      media.find(m => ['sm', 'xs'].some(v => v === m.mqAlias)) ?
+      ['activity', 'submit'] :
+      ['activity', 'points', 'location', 'submit']
+    ));
+
+    this.displayedColumns$.subscribe(c => console.log(c))
+
     this.dataSource.sort = this.sort;
     this.dataSource.filterPredicate = (data) => {
       return (this.activityFilter ? data.activity.toLowerCase().trim().includes(this.activityFilter.toLowerCase().trim()) : true) &&
